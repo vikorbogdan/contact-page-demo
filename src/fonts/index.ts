c73ac267f6d4ba52a5a38326ab38cde1e83b077d
Vikor Bogdán <vikorbogdan@gmail.com>
2024-08-28 19:03:03 +0200

    feat(init): Initialized Next.JS Project with base dependencies
    - Initialized Next.JS with app router and TailwindCSS
    - Added fonts as variables to next.js project using next/font/local
    - Initialized Prisma and Prisma Client with SQLite
    - Added dependencies: sqlite3, prisma

---
+++ src/fonts/index.ts
@@ -0,0 +1,10 @@
+import localFont from "next/font/local";
+
+export const Glysa = localFont({
+  src: "./Glysa.otf",
+  variable: "--font-glysa",
+});
+export const LexendDeca = localFont({
+  src: "./LexendDeca.ttf",
+  variable: "--font-lexend-deca",
+});
